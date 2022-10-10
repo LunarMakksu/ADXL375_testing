5f8e3cbf7793db1851ac18d603a8ae1a75929fbe
mlbird1 <mlbird1@sheffield.ac.uk>
2022-10-10 21:13:35 +0100

    added constants h

---
+++ ADXL375-library/constants.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Max Bird on 10/10/2022.
+//
+
+#include "constants.h"
--- ADXL375-library/main.cpp
+++ ADXL375-library/main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+// Created by Max Bird on 10/10/22
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+#include <iostream>
+#ifndef ADXL375_H
+#define ADXL375_H
+
+#include "protocol.h"
+#include "Vector.h"
+#include "ADXL375_registers.h"
+#include "ADXL375_constants.h"
+
+class ADXL375{
+    protected:
+        protocol *device;
+    public:
+        ADXL375(TwoWire *pipe, uint32_t freq) { // constructor for I2C protocol
+            device = new I2CProtocol(ADXL375_DEFAULT_I2C_ADDRESS, pipe, freq);
+        }
+
+        ADXL375(byte chipSelect, SPIClass& spi, SPISettings settings) {// constructor overload for SPI protocol
+            device = new SPIProtocol(chipSelect, spi, settings, READ_BYTE, WRITE_BYTE);
+        }
+};
